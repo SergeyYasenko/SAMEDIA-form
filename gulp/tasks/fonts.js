a5e6019970d1e3d8407b80a7067e410c87c4ba3a
Сергей <91138847+SergeyYasenko@users.noreply.github.com>
2024-07-29 19:15:28 +0300

    bug fix
    bug fix

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import fonter from 'gulp-fonter';
+import fonter from 'gulp-fonter-fix';
+import ttf2woff from 'gulp-ttf2woff';
 import ttf2woff2 from 'gulp-ttf2woff2';
 
 
@@ -13,32 +14,28 @@ export const otfToTtf = () => {
          })))
       // Конвертируем в .ttf
       .pipe(fonter({
-      formats: ['ttf']
+         formats: ['ttf']
       }))
-   // Выгружаем в исходную папку
-   .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
+      // Выгружаем в исходную папку
+      .pipe(app.gulp.dest(`${app.path.srcFolder}/fonts/`))
 }
+
 
 export const ttfToWoff = () => {
    // Ищем файлы шрифтов .ttf
-   return app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`, {})
-      .pipe(app.plugins.plumber(
-         app.plugins.notify.onError({
-            title: "FONTS",
-            message: "Error: <%= error.message %>"
-         })))
-      // Конвертируем в .woff
-      .pipe(fonter({
-         formats: ['woff']
+   return app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`)
+      .pipe(app.plugins.plumber({
+         errorHandler: function (err) {
+            // Обработчик ошибок, исключая наличие нулевых байтов в сообщении
+            const errorMessage = err.message.replace(/\x00/g, ''); // убираем нулевые байты
+            console.log('Error message without null bytes:', errorMessage);
+         }
       }))
-      // Выгружаем в папку с результатом
-      .pipe(app.gulp.dest(`${app.path.build.fonts}`))
-      // Ищем файлы шрифтов .ttf
+      .pipe(ttf2woff())
+      .pipe(app.gulp.dest(`${app.path.build.fonts}`))  // Сохраняем в формате WOFF
       .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
-      // Конвертируем в .woff2
       .pipe(ttf2woff2())
-      // Выгружаем в папку с результатом
-      .pipe(app.gulp.dest(`${app.path.build.fonts}`));
+      .pipe(app.gulp.dest(`${app.path.build.fonts}`));  // Сохраняем в формате WOFF2 
 }
 
 export const fontsStyle = () => {
